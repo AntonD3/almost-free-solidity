67af1638c0e2bb0768abbe03382c85d1ce0c6869
AntonD3 <antonv.dyadyuk@gmail.com>
2024-11-16 14:04:23 +0700

    ComputationOracle contract

--- contracts/src/ComputationOracle.sol
+++ contracts/src/ComputationOracle.sol
@@ -1,10 +1,21 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
 contract ComputationOracle {
-    mapping(bytes32 merkleRoot -> bool proved) proofs;
+    using MerkleProof for bytes32[];
 
-    function verifyComputation(bytes32 codeHash, bytes32 input, bytes32 output) {
+    mapping(bytes32 merkleRoot => bool proved) proved;
 
+    function verifyComputation(bytes32 codeHash, bytes32 inputHash, bytes32 outputHash, bytes32[] calldata proof) external view {
+        bytes memory computationProofPublicInput = bytes.concat(codeHash, inputHash, outputHash);
+        bytes32 leaf = keccak256(computationProofPublicInput);
+        require(proved[proof.processProof(leaf)]);
+    }
+
+    function proveComputation(bytes32 merkleRoot, bytes memory zkp) external {
+        proved[merkleRoot] = true;
+        // TODO: verify zkp proof
     }
 }
