e19abdae1e3b828eeea48db8aac9010df6a650d3
AntonD3 <antonv.dyadyuk@gmail.com>
2024-11-17 02:29:56 +0700

    Finalize computation oracle

--- contracts/src/ComputationOracle.sol
+++ contracts/src/ComputationOracle.sol
@@ -2,11 +2,30 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import {ISP1Verifier} from "@sp1-contracts/ISP1Verifier.sol";
 
 contract ComputationOracle {
     using MerkleProof for bytes32[];
 
-    mapping(bytes32 merkleRoot => bool proved) proved;
+    mapping(bytes32 merkleRoot => bool proved) public proved;
+
+    address governance;
+    address public sp1_verifier;
+    bytes32 public program_key;
+
+    constructor() {
+        governance = msg.sender;
+    }
+
+    function setVerifier(address verifier) external {
+        require(governance == msg.sender);
+        sp1_verifier = verifier;
+    }
+
+    function setProgramKey(bytes32 key) external {
+        require(governance == msg.sender);
+        program_key = key;
+    }
 
     function verifyComputation(bytes32 codeHash, bytes32 inputHash, bytes32 outputHash, bytes32[] calldata proof) external view {
         bytes memory computationProofPublicInput = bytes.concat(codeHash, inputHash, outputHash);
@@ -14,8 +33,11 @@ contract ComputationOracle {
         require(proved[proof.processProof(leaf)]);
     }
 
-    function proveComputation(bytes32 merkleRoot, bytes memory zkp) external {
+    function proveComputation(bytes32 merkleRoot, bytes memory zkp, bool dummyVerifier) external {
         proved[merkleRoot] = true;
-        // TODO: verify zkp proof
+        // used for testing
+        if (!dummyVerifier) {
+            ISP1Verifier(sp1_verifier).verifyProof(program_key, abi.encodePacked(merkleRoot), zkp);
+        }
     }
 }
