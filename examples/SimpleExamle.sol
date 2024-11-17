2b5b2e37f3d51a1f360364d6187a13ddf3efab90
AntonD3 <antonv.dyadyuk@gmail.com>
2024-11-17 07:07:51 +0700

    Improve example

--- examples/SimpleExamle.sol
+++ examples/SimpleExamle.sol
@@ -3,12 +3,13 @@
 pragma solidity >=0.8.2 <0.9.0;
 
 contract Example {
-    function func() public returns (uint256){
-        return sum(100);
+    function example_func() external returns (uint256){
+        return heavy_computation(100);
     }
 
+    // This function will not be computed on chain! The output will be verified using ZK!
     @free
-    function sum(uint256 n) internal returns(uint256 result) {
+    function heavy_computation(uint256 n) internal returns(uint256 result) {
         result = 0;
         for (uint256 i = 1; i <= n; i++) {
             result += i;
